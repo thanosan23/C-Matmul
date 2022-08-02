2af866b7d4f9d1819d605a9707a810799e7536a3
thanosan23 <79427186+thanosan23@users.noreply.github.com>
2022-08-02 08:53:46 -0400

    Added include guards

--- profile.h
+++ profile.h
@@ -1,3 +1,6 @@
+#ifndef C_PROFILE_H
+#define C_PROFILE_H
+
 #include <time.h>
 double diff(struct timespec* t1, struct timespec* t2) {
   return ((t1->tv_sec - t2->tv_sec) * 1e9 + (t1->tv_nsec - t2->tv_nsec))/1e9;
@@ -8,3 +11,5 @@ struct timespec current_time() {
   clock_gettime(CLOCK_MONOTONIC_RAW, &t);
   return t;  
 }
+
+#endif
